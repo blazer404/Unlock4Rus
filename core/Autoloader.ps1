3bea4bfa75939a5085d58b7ab17f02d826c7564a
SergeyT <0tx0rx@gmail.com>
2025-08-21 14:15:07 +0700

    загрузка/выгрузка модулей: отключение вывода имен в лог

--- core/Autoloader.ps1
+++ core/Autoloader.ps1
@@ -5,5 +5,5 @@ $modules = Get-ChildItem -Path "$PSScriptRoot\classes" -Recurse -Filter *.ps1 -E
 $modules | ForEach-Object {
     Get-Module | Where-Object { $_.Name -like "*$( $_.Name )*" } | Remove-Module -Force -ErrorAction Stop
     Import-Module $_.FullName -Force -ErrorAction Stop
-    Write-Host "  $( $_.Name )" -ForegroundColor Gray
+    # Write-Host "  $( $_.Name )" -ForegroundColor Gray
 }
--- core/Unloader.ps1
+++ core/Unloader.ps1
@@ -4,6 +4,6 @@ $modules = Get-ChildItem -Path "$PSScriptRoot\" -Recurse -Filter *.ps1 -ErrorAct
 
 $modules | ForEach-Object {
     Get-Module | Where-Object { $_.Name -like "*$( $_.Name )*" } | Remove-Module -Force -ErrorAction Stop
-    Write-Host "  $( $_.Name )" -ForegroundColor Gray
+    # Write-Host "  $( $_.Name )" -ForegroundColor Gray
 }
 
