0a1da0687322b44b203450539cb43335129c6c61
SergeyT <0tx0rx@gmail.com>
2025-10-06 22:43:33 +0700

    замена ссылки на hosts от ImMALWARE

--- core/modules/Constants.ps1
+++ core/modules/Constants.ps1
@@ -1,3 +1,3 @@
-[ConstantCreator]::create("HOSTS_SRC", "https://raw.githubusercontent.com/AvenCores/Unlock_AI_and_EN_Services_for_Russia/refs/heads/main/source/system/etc/hosts")
+[ConstantCreator]::create("HOSTS_SRC", "https://raw.githubusercontent.com/ImMALWARE/dns.malw.link/refs/heads/master/hosts")
 [ConstantCreator]::create("HOSTS_DST", ".\out\hosts")
 [ConstantCreator]::create("STATIC_DNS_DST", ".\out\static_dns.rsc")
