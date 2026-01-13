6a30a339fc6be4f6e42495c4a1d69ff93af39d9a
SergeyT <0tx0rx@gmail.com>
2026-01-13 23:31:16 +0700

    отказ от класса констант. просто создаем переменные (от красивого создания больше проблем)

--- core/modules/Constants.ps1
+++ core/modules/Constants.ps1
@@ -1,3 +1,6 @@
-[ConstantCreator]::create("HOSTS_SRC", "https://raw.githubusercontent.com/ImMALWARE/dns.malw.link/refs/heads/master/hosts")
-[ConstantCreator]::create("HOSTS_DST", ".\out\hosts")
-[ConstantCreator]::create("STATIC_DNS_DST", ".\out\static_dns.rsc")
+$HOSTS_SRC = "https://raw.githubusercontent.com/ImMALWARE/dns.malw.link/refs/heads/master/hosts"
+
+$HOSTS_DST = ".\out\hosts"
+
+$STATIC_DNS_DST = ".\out\static_dns.rsc"
+
