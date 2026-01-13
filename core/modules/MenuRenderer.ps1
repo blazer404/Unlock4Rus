9da08258be41f37b2e0484fc33b8db6e7058a877
SergeyT <0tx0rx@gmail.com>
2026-01-14 00:26:20 +0700

    переименование главного меню

--- core/modules/MenuRenderer.ps1
+++ core/modules/MenuRenderer.ps1
@@ -4,10 +4,11 @@ class MenuRenderer {
     [Void]
     show() {
         Write-Host "- Choose what you want:"
-        [MenuEntry]::new("1", "Convert only", "unblock").show()
-        [MenuEntry]::new("2", "Convert only", "block").show()
-        [MenuEntry]::new("3", "Convert", "both").show()
-        [MenuEntry]::new("4", "Only download", "hosts").show()
+
+        [MenuEntry]::new("1", "Convert", "unblocking").show()
+        [MenuEntry]::new("2", "Convert", "blocking").show()
+        [MenuEntry]::new("3", "Convert", "all").show()
+        [MenuEntry]::new("4", "Download", "hosts").show()
         [MenuEntry]::new("q", "Exit").show()
     }
 
